ce2905b4cd17e02da36c8221abc30e84567d0638
aryank730 <rikkypatel730@gmail.com>
2025-04-19 22:58:59 +0530

    assets added

---
+++ src/assets/assets.js
@@ -0,0 +1,27 @@
+import image from './images/image.png'; 
+
+export const assets = {
+   image: image,
+   
+}
+
+export const products = [
+      {
+         id: 1,
+         name: 'Product 1',
+         price: 10.00,
+         image: image,
+      },
+      {
+         id: 2,
+         name: 'Product 2',
+         price: 20.00,
+         image: image,
+      },
+      {
+         id: 3,
+         name: 'Product 3',
+         price: 30.00,
+         image: image,
+      },
+]
