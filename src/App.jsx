14ccfde3bc947b4d8c96b1664161bef0bea579ee
aryank730 <rikkypatel730@gmail.com>
2025-04-27 23:35:18 +0530

    nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Cart from './Pages/Cart'
+import PlaceOrder from './Pages/PlaceOrder'
+// import NotFound from './Pages/NotFound'
+import Products from './Pages/Product'
+import Collection from './Pages/Collection'
+import Login from './Pages/Login'
+import Contact from './Pages/Contact'
+import Register from './Pages/Register'
+import Orders from './Pages/Orders' 
+import Navbar from './Pages/Navbar'
 
 function App() {
 
   return (
     <>
-     
+    <Navbar/>
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/placeorder" element={<PlaceOrder />} />
+      <Route path="Collection" element={<Collection />} />
+      <Route path="/products:Productid" element={<Products />} />
+      <Route path="/Contact" element={<Contact />} />
+      <Route path="/Orders" element={<Orders />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} /> 
+      
+      {/* <Route path="/product/:id" element={<Product />} />
+      <Route path="/products" element={<Products />} />
+       <Route path="/products/:category" element={<Products />} />
+      <Route path="/products/:category/:subCategory" element={<Products />} />
+      <Route path="/products/:category/:subCategory/:subSubCategory" element={<Products />} /> */}
+    </Routes>
     </>
   )
 }
--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Routing() {
+const Navbar = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ function Routing() {
   )
 }
 
-export default Routing
+export default Navbar
